b98a69145c74d32380ec7d9c2c95190e46c45c22
jordans6 <jordanschneider@telus.net>
2022-01-15 15:04:04 -0800

    update on yelp api

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -5,6 +5,25 @@ const LOCATION = "Seattle";
 const CATEGORY = "restaurant";
 const API_KEY = 'rZz4DQe06UyMRcTToBVyQrOrkltbyZ5F8MvO3yW8vadGOo41iG8PZKoi_-HWV4p7LVSIOnXIctifKNQf_1sSsmbP1RkWKnAufcpA5p65jU4a4zSmqX03dzP_cfPgYXYx'
 
+/* for yelp api, the following attributes from the api will be used:
+name
+review_count
+rating
+coordinates - for google distance matrix
+location -> display
+price
+phone
+*/
+
+/* for yelp api, the following terms will be available to the user:
+PARKS ->  
+ENTERTAINMENT ->
+TOURS -> maybe use, could just give a list of potential tours but not include in itinerary
+NIGHTLIFE -> 
+RESTAURANTS ->
+SHOPPING -> 
+*/
+
 router.route('/').get((req, res) => {
   var bearer = 'Bearer ' + API_KEY;
   var request_string = "https://api.yelp.com/v3/businesses/search" + "?term=" + CATEGORY + "&location=" + LOCATION;
@@ -20,7 +39,8 @@ router.route('/').get((req, res) => {
 
   axios(config)
     .then(function (response) {
-      console.log(JSON.stringify(response.data));
+      data = JSON.parse(JSON.stringify(response.data));
+      console.log(data.businesses[0]);
     })
     .catch(function (error) {
       console.log(error);
