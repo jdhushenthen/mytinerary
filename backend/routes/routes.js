553e6ecd88d5a3263aeb51ccdbb2cd8dc8a17984
Janahan Dhushenthen <jdhushenthen@rogers.com>
2022-01-15 17:06:10 -0500

    working Yelp API

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,23 +4,27 @@ var axios = require('axios');
 const LOCATION = "Seattle";
 const CATEGORY = "restaurant";
 const API_KEY = 'rZz4DQe06UyMRcTToBVyQrOrkltbyZ5F8MvO3yW8vadGOo41iG8PZKoi_-HWV4p7LVSIOnXIctifKNQf_1sSsmbP1RkWKnAufcpA5p65jU4a4zSmqX03dzP_cfPgYXYx'
-var bearer = 'Bearer ' + API_KEY;
-
-var request_string = "https://api.yelp.com/v3/businesses/search" + "?term=" + CATEGORY + "&location=" + LOCATION;
 
 router.route('/').get((req, res) => {
-    console.log("API call")
+  var bearer = 'Bearer ' + API_KEY;
+  var request_string = "https://api.yelp.com/v3/businesses/search" + "?term=" + CATEGORY + "&location=" + LOCATION;
+  var config = {
+    method: 'get',
+    url: request_string,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Authorization': bearer,
+      'Content-Type': 'application/json',
+    }
+  };
 
-    fetch(request_string, {
-      method: 'GET',
-      headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Authorization': bearer,
-          'Content-Type': 'application/json',
-      }
-  })
-  .then(res => console.log(res))
-  .catch(err => console.log(err))
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 })
 
 module.exports = router;
